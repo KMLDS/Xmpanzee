f95b09474e90f271e0a86a64d664c4e332122b53
Kevin Lyons <kevin.lyons.physics@gmail.com>
2017-02-21 21:44:52 -0500

    refactored ArXivPage to take arbitrary param list

--- src/main/scala/Xmpanzee.scala
+++ src/main/scala/Xmpanzee.scala
@@ -3,7 +3,17 @@ import xmpanzee.arxivdata._
 object Xmpanzee {
   def main(args: Array[String]) = {
     if (args(0) == "download") {
-      val testPage = ArXivPage(args(1), args(2), args(3))
+
+      val argList = args.toList.tail
+      def argsToPairs(lst: List[String]): List[(String, String)] = {
+        lst match {
+          case Nil => Nil
+          case x::y::zs => (x, y)::argsToPairs(zs)
+          case _ => throw new Exception("Odd number of arguments, needs to be key-value pairs")
+        }
+      }
+
+      val testPage = ArXivPage(argsToPairs(argList))
       val testRecords = (testPage.text \ "ListRecords" \ "record").map(new ArXivRecord(_))
       println(testRecords)
     }
--- src/main/scala/arxivdata/ArXivPage.scala
+++ src/main/scala/arxivdata/ArXivPage.scala
@@ -3,56 +3,30 @@ package xmpanzee.arxivdata
 import scalaj.http._
 import scala.xml._
 
-/** ArXivPage takes Option[String] arguments corresponding to the "set" (e.g. physics:quant-ph) variable, and the from and until dates in the format 
-  YYYY-MM-DD.
+/** ArXivPage takes Seq[(String, String)] key-value pairs to pass directly to scalaj.http.Http.params() .
 
   The text field contains the entire XML of the page corresponding to the parameters given.  It needs to properly handle timeouts, other server side errors (or blocks), and 
-  resumption tokens.
+  resumption tokens.  Some useful key-value pairs include ("verb", "Listrecords"), ("metadataPrefix", "arXiv"), ("from", "YYYY-MM-DD"), ("until", "YYYY-MM-DD"), and ("set", "physics:quant-ph") for example.
 
   */
-class ArXivPage(arXivSet: Option[String] = None, dateFrom: Option[String] = None, dateUntil: Option[String] = None) {
-
+class ArXivPage(httpParams: Seq[(String, String)]) {
 
   val arXivBaseURL = "http://export.arxiv.org/oai2"
-  val metaDataFormat = "arXiv"
-  val downloadVerb = "ListRecords"
-
-  def buildParamList(): List[(String, String)] = {
-    def prependOptional(x: Option[String], optKey: String, lst: List[(String, String)]): List[(String, String)] = {
-      x match {
-        case Some(y) => (optKey, y)::lst
-        case None => lst
-      }
-    }
-    val baseList = List(("verb", downloadVerb), ("metadataPrefix", metaDataFormat))
-    prependOptional(dateFrom, "from", prependOptional(dateUntil, "until", prependOptional(arXivSet, "set", baseList)))
-  }
 
   def getArXivXML(): scala.xml.Elem = {
-    val paramList = this.buildParamList
-    val pageString: String = Http(arXivBaseURL).params(paramList).asString.body
+    val pageString: String = Http(arXivBaseURL).params(httpParams).asString.body
     scala.xml.XML.loadString(pageString)
   }
 
-  // private def getRecords(): List[scala.xml.Elem] = {
-  //   this.text match {
-
-  //   }
-  // }
-
-
   val text = this.getArXivXML
 }
+
 
 /** Companion object for class ArXivPage.  Can take all 3 parameters (set, from date, until date), just the set parameter, or no parameter.
   
   */
 object ArXivPage {
 
-
-  def apply(arXivSet: String, dateFrom: String, dateUntil: String) = new ArXivPage(Some(arXivSet), Some(dateFrom), Some(dateUntil))
-  def apply(arXivSet: String) = new ArXivPage(Some(arXivSet))
-  def apply() = new ArXivPage()
-
+  def apply(httpParams: Seq[(String, String)]) = new ArXivPage(httpParams)
 
 }
